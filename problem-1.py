2c6f125919ae5b0c10123e7f407c5a02f0ad93d4
Praveen Bathula <praveenbathula7@gmail.com>
2022-12-30 13:36:00 +0530

    sum of multiples of 3 and 5 below 1000

---
+++ problem-1.py
@@ -0,0 +1,49 @@
+"""
+Multiples of 3 or 5
+
+Problem 1
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+"""
+import functools
+import itertools
+
+from util import benchmark
+
+
+@benchmark
+def normal(n: int) -> int:
+    sum_n = 0
+    for i in range(n + 1):
+        if i % 3 == 0 or i % 5 == 0:
+            sum_n += i
+    return sum_n
+
+
+@benchmark
+def method_2(n: int) -> int:
+    filtered = filter(lambda x: x % 3 == 0 or x % 5 == 0, range(n + 1))
+    return sum(filtered)
+
+
+@benchmark
+def optimized(n: int) -> int:
+    divisors = [3, 5]
+    sum_n = 0
+
+    for i in range(len(divisors)):
+        comb = itertools.combinations(divisors, i + 1)
+        for each in comb:
+            value = functools.reduce(lambda x, y: x * y, each)
+            count = n // value
+            sum_n += (1 if len(each) % 2 != 0 else -1) * (value * (count * count + count) // 2)
+    return sum_n
+
+
+number = 99999
+
+print(normal(number))
+print(method_2(number))
+print(optimized(number))
